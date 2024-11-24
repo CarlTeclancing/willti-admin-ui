bc8e77a34ca3ce61af8bbf853dc19b13f88683b1
YuvCarl <carldrake969@gmail.com>
2024-11-24 19:08:39 +0100

    added the booking section tab to user detials

--- src/dashboard/users/UserDetails.jsx
+++ src/dashboard/users/UserDetails.jsx
@@ -1,15 +1,160 @@
 import React from 'react'
 import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import SideBar from "../../components/SideBar"
 import TopBar from "../../components/TopBar"
 import User from '../../assets/icons/user.svg'
 import CarImg from '../../assets/icons/car.png'
-import TuggleSwitch from '../../components/TuggleSwitch'
+import Rating from '../../assets/icons/vip.svg'
+import Bus from '../../assets/icons/bus.png'
 import Button from '../../components/Button'
 
 export default function UserDetails() {
+const rides = [
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
 
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    },
+    {
+        time: "Morning ride at 10:40",
+        start: "Entre simbock",
+        end: "Dovv Mendong",
+        price: 1700,
+        car:CarImg
+
+    }
+]
+//object with bookings data
+
+const bookings = [
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    {
+        id:"1111234",
+        agency: "Vatican Express",
+        dep: "Yaounde",
+        dest: "Bamenda",
+        price: 6500,
+        carType: Rating,
+        car:Bus
+    },
+    
+]
+//assessing state from the url location variable
     
     const location = useLocation()
     const { user } = location.state || {} // Destructure item from state
@@ -19,6 +164,30 @@ export default function UserDetails() {
         setTurgle(index)
     }
     if (!user) return <p>No user data found.</p>; 
+
+    
+    const [currentPage, setCurrentPage] = useState(0);
+    const ridesPerPage = 10;
+
+    //to ident the row colors 
+    var count = 0;
+
+      // Slice the rides array to show only the rides for the current page
+  const currentRides = rides.slice(currentPage * ridesPerPage, (currentPage + 1) * ridesPerPage);
+
+  const handleNext = () => {
+    // Make sure we don't go past the last page
+    if ((currentPage + 1) * ridesPerPage < rides.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div className="container">
 
@@ -26,7 +195,7 @@ export default function UserDetails() {
         <div className="narrow">
             <TopBar PageTitle='Profile'/>
             <div className="column">
-                <div className="row">
+                <div className="row-fluid">
                     <div className="tabs">
                         <div 
                             className={turgle === 1?'tab-el active-tab':'tab-el'}
@@ -109,9 +278,10 @@ export default function UserDetails() {
                         className={turgle ===2?'tab-content':'none'}
                     >
                         <h4>Rides Hostory</h4>
-                        <div className="row-fluid">
+                        
                             <table className="table">
                                 <tr>
+                                    <td>Id</td>
                                     <td>Time</td>
                                     <td>From</td>
                                     <td>To</td>
@@ -119,17 +289,31 @@ export default function UserDetails() {
                                     <td>Car</td>
                                     <td>Action</td>
                                 </tr>
+                                {currentRides.map((rideData)=>(
 
-                                <tr className="table-r1">
-                                <td>Time</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.name}</td>
-                                    <td><span className="verify">{user.name}</span></td>
-                                    <td><img src={CarImg} alt="" /></td>
-                                    <td>{user.name}</td>
+                                <tr className={count % 2 === 0 ?'table-r1':'table-r2'}>
+                                    <td>{count +=1}</td>
+                                    <td>{rideData.time}</td>
+                                    <td>{rideData.start}</td>
+                                    <td>{rideData.end}</td>
+                                    <td><span className="verify">{rideData.price}</span></td>
+                                    <td><img src={rideData.car} alt="" /></td>
+                                    <td>
+                                        <Link 
+                                            to='/dashboard/users/user-detials/ride-details' 
+                                            state={{ user, rideData }} 
+                                            className='btn-outline'
+                                        >
+                                            View More
+                                        </Link></td>
                                 </tr>
+                                ))}
                             </table>
-                        </div>
+                            <div className='pagination'>
+                                <button onClick={handlePrevious} disabled={currentPage === 0} className='btn-outline'>Previous</button>
+                                <button onClick={handleNext} disabled={(currentPage + 1) * ridesPerPage >= rides.length} className='btn-outline'>Next</button>
+                            </div>
+                        
 
                     </div>
 
@@ -137,6 +321,46 @@ export default function UserDetails() {
                         className={turgle ===3?'tab-content':'none'}
                     >
                         <h4>Booking Hostory</h4>
+                        <div className="row-fluid">
+                            <table className="table">
+                                <tr>
+                                    <td>Id</td>
+                                    <td>Transaction Id</td>
+                                    <td>Agency</td>
+                                    <td>Departure</td>
+                                    <td>Destination</td>
+                                    <td>Price</td>
+                                    <td>Car Type</td>
+                                    <td>Car</td>
+                                    <td>Action</td>
+                                </tr>
+                                {bookings.map((booking)=>(
+
+                                <tr className="table-r1">
+                                    <td>{count +=1}</td>
+                                    <td>{booking.id}</td>
+                                    <td>{booking.agency}</td>
+                                    <td>{booking.dep}</td>
+                                    <td>{booking.dest}</td>
+                                    <td><span className="verify">{booking.price}</span></td>
+                                    <td><img src={booking.carType} alt="" /></td>
+                                    <td><img src={booking.Bus} alt="" /></td>
+                                    <td>
+                                        <Link 
+                                            to='/dashboard/users/booking-details' 
+                                            state={{ user, booking }} 
+                                            className='btn-outline'
+                                        >
+                                            View More
+                                        </Link></td>
+                                </tr>
+                                ))}
+                            </table>
+                            <div className='pagination'>
+                                <button onClick={handlePrevious} disabled={currentPage === 0} className='btn-outline'>Previous</button>
+                                <button onClick={handleNext} disabled={(currentPage + 1) * ridesPerPage >= rides.length} className='btn-outline'>Next</button>
+                            </div>
+                        </div>
 
                     </div>
 
